ef02ff474d5fcd7277012658b4065874924f1c88
Skuiggly <piplup107@gmail.com>
2022-05-25 23:32:03 +0200

    feat: css styling
    chore: add checked styling
    updated manifest.json and index.html theme colors

--- src/components/RatingWindow.tsx
+++ src/components/RatingWindow.tsx
@@ -3,6 +3,8 @@ import { useState} from "react"
 import {ReactComponent as StarIcon} from "../images/icon-star.svg"
 import {ReactComponent as ThanksIcon} from "../images/illustration-thank-you.svg"
 
+import "../css/rating-window.css"
+
 const RatingWindow = () => {
   const [submitted, setSubmitted] = useState(false);
   const toggleSubmit = () => setSubmitted(!submitted);
@@ -15,16 +17,19 @@ const RatingWindow = () => {
   
   const ratingButtons = [1, 2, 3, 4, 5].map((n) => {
     return (
-      <div key={n}>
+      <label key={n}>
         <input type="radio" value={n} name="rating" required onClick={() => changeSelectedRating(n)}/>
-        <label>{n}</label>
-      </div>
+        <span>{n}</span>
+      </label>
     )
   })
   if (!submitted) {
    return (
-    <div>
-      <StarIcon />
+    <div className="container">
+      <div className="star-icon">
+        {/* Couldn't find a better way to center the SVG than playing with viewbox */}
+        <StarIcon viewBox="-16 -16 50 50"/>
+      </div>
       <h2>How did we do?</h2>
       <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       <form>
@@ -34,17 +39,17 @@ const RatingWindow = () => {
             toggleSubmit();
           }
         }}
-        value="Submit"/>
+        value="SUBMIT"/>
         </form>
     </div>
     )
   } else {
     return (
-      <div>
+      <div className="container thanks">
         <ThanksIcon />
-        <span>
-          <p>You selected {selectedRating} out of 5</p>
-        </span>
+        <p className="rating-text">
+          You selected {selectedRating} out of 5
+        </p>
         <h2>Thank you!</h2>
         <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
       </div>
