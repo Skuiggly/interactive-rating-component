aaf5d68999d8be943922528da26a1a8f27b5be02
Skuiggly <piplup107@gmail.com>
2022-06-17 06:15:51 +0200

    refactor(RatingWindow): make code more readable

--- src/components/RatingWindow.tsx
+++ src/components/RatingWindow.tsx
@@ -6,24 +6,19 @@ import {ReactComponent as ThanksIcon} from "../images/illustration-thank-you.svg
 import "../css/rating-window.css"
 
 const RatingWindow = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const toggleSubmit = () => setSubmitted(!submitted);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
 
   const [selectedRating, setSelectedRating] = useState(0);
-  const changeSelectedRating = (n:number) => {
-    setSelectedRating(n);
-    console.log(n);
-  }
-  const possibleRatings =[1, 2, 3, 4, 5];
-  const ratingButtons = possibleRatings.map((n) => {
+  const ratingRange = [1, 2, 3, 4, 5];
+  const ratingButtons = ratingRange.map((n) => {
     return (
       <div key={n}>
-        <input value={n} onClick={() => changeSelectedRating(n)} id={n.toString()} name="rating" type="radio" required/>
+        <input value={n} onClick={() => setSelectedRating(n)} id={n.toString()} name="rating" type="radio" required/>
         <label htmlFor={n.toString()}>{n}</label>
       </div>
     )
   })
-  if (!submitted) {
+  if (!hasSubmitted) {
    return (
     <div className="container">
       <div className="star-icon">
@@ -34,24 +29,21 @@ const RatingWindow = () => {
       <p className="text">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       <form>
         {ratingButtons}
-        <input type="submit" onClick={() => {
-          if (selectedRating) {
-            toggleSubmit();
-          }
-        }}
+        <input type="submit" onClick={() => selectedRating ?
+          setHasSubmitted(true) : console.log('Nothing to submit')}
         value="SUBMIT"/>
-        </form>
+      </form>
     </div>
     )
   } else {
     return (
       <div className="container thanks">
         <ThanksIcon />
-        <p className="text rating-text">
+        <p className="rating-text">
           You selected {selectedRating} out of 5
         </p>
         <h2>Thank you!</h2>
-        <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+        <p className="text">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
       </div>
     )
   }
